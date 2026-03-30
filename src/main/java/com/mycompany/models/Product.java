a4d697247552029b3f9501e4599c3f4337b458e6
Nguyen Van Phong <phongcho060207@gmail.com>
2026-03-30 15:30:34 +0700

    reup

---
+++ src/main/java/com/mycompany/models/Product.java
@@ -0,0 +1,13 @@
+package com.mycompany.models;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private String name;
+    private String id;
+    private static final long serializeVersionUID = 1L;
+    public Product(String id, String name){
+
+    }
+
+}
