93e40a1a02d77465b2c7a1a166e41f72ce28535d
Nguyen Van Phong <phongcho060207@gmail.com>
2026-04-06 12:00:03 +0700

    Sua ten chuan

--- src/main/java/com/mycompany/models/Main.java
+++ src/main/java/com/mycompany/models/Main.java
@@ -14,21 +14,22 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) {
-        User seller = new User("phongvan");
-        User buyer1 = new User("phongnguyen");
-        User buyer2 = new User("happyboy");
-        Product luv = new Product("001", "Ao da");
+        NguoiDung seller = new NguoiDung("phongvan");
+        NguoiDung buyer1 = new NguoiDung("phongnguyen");
+        NguoiDung buyer2 = new NguoiDung("happyboy");
+        SanPham luv = new SanPham("001", "Ao da");
         PhienDauGia phienDauGia = new PhienDauGia("P01", "Dau gia ao luv", luv, (double)1.0E7F, seller);
         phienDauGia.batDauPhien();
         phienDauGia.setOnAutionFinished(() -> {
             System.out.println("\n--- KẾT QUẢ PHIÊN ĐẤU GIÁ ---");
-            System.out.println("Người chiến thắng: " + phienDauGia.getWinner().getName());
-            System.out.printf("Giá chốt: %,.0f VNĐ\n", phienDauGia.getGiaChot());
-            System.out.println("Tổng thời gian diễn ra: " + phienDauGia.getThoiGianDauGia());
+            System.out.println("Người chiến thắng: " + phienDauGia.layNguoiThangCuoc().layHoTen());
+            System.out.printf("Giá chốt: %,.0f VNĐ\n", phienDauGia.layGiaChot());
+            System.out.println("Tổng thời gian diễn ra: " + phienDauGia.layThoiGianDauGia());
             GiaoDich giaoDich = new GiaoDich("001", phienDauGia);
             System.out.println(giaoDich.toString());
             Platform.exit();
         });
+        /// 1 2 3 4 5 6 7 8 9
         Timeline timeline = new Timeline(new KeyFrame[]{new KeyFrame(Duration.seconds((double)4.0F), (e) -> {
             System.out.println("Nguoi A tra gia 15000000");
             phienDauGia.nguoiChoiTraGia(buyer1, (double)1.5E7F);
---
+++ src/main/java/com/mycompany/models/SanPham.java
@@ -0,0 +1,21 @@
+package com.mycompany.models;
+
+import java.io.Serializable;
+
+public class SanPham implements Serializable {
+    private String tenSanPham;
+    private final String maSanPham;
+    public SanPham(String tenSanPham, String maSanPham) {
+        this.tenSanPham = tenSanPham;
+        this.maSanPham = maSanPham;
+    }
+    public String layTenSanPham() {
+        return this.tenSanPham;
+    }
+    public void suaTenSanPham(String tenSanPham) {
+        this.tenSanPham = tenSanPham;
+    }
+    public String layMaSanPham() {
+        return this.maSanPham;
+    }
+}
