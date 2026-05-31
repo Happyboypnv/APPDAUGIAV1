a3112c7eebdb2b49c05e5113ad0849ee6571e6f7
TrungHoang07 <hgtrung07@gmail.com>
2026-05-31 20:48:07 +0700

    remove address format

--- client/src/main/java/com/mycompany/action/ProfileAction.java
+++ client/src/main/java/com/mycompany/action/ProfileAction.java
@@ -102,16 +102,6 @@ public class ProfileAction {
         // {10,15}: độ dài từ 10 đến 15 chữ số
         // $: kết thúc chuỗi
         if (!phoneNumber.matches(phoneRegex)) throw new PhoneNumberException("Số điện thoại không hợp lệ!");
-
-        if (address == null || address.isEmpty()) throw new AddressException("Địa chỉ đang bỏ trống!");
-        String addressRegex = "^[^,]+,\\s*[^,]+$"; // <--- Chỉ dùng 2 dấu gạch chéo
-        // ^: bắt đầu chuỗi
-        // p{L} : cho phep ngon ngu tieng Viet
-        // [^,]+: mỗi phần phải có ít nhất 1 ký tự và không chứa dấu phẩy
-        // ,\\s*: cho phép có hoặc không có khoảng trắng sau dấu phẩy
-        // $: kết thúc chuỗi
-        // Yêu cầu địa chỉ phải đúng nhất 2 phần (xã + thành phố), mỗi phần không chứa dấu phẩy, và được phân tách bằng một dấu phẩy (có thể có khoảng trắng sau dấu phẩy)
-        if (!address.matches(addressRegex)) throw new AddressException("Địa chỉ không hợp lệ! Vui lòng nhập đúng định dạng: 'Xã, Thành phố'");
     }
 
     /**
